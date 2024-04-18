73410dcb5d65338d796a35a91d5dba9c6de56f8b
ederperedalo <156089611+ederperedalo@users.noreply.github.com>
2024-04-18 14:11:25 +0200

    Update Database_Grupo6_Reto4.sql

--- Database/Database_Grupo6_Reto4.sql
+++ Database/Database_Grupo6_Reto4.sql
@@ -2,8 +2,8 @@ create database IF NOT EXISTS reto4_grupo6;
 use reto4_grupo6;
 
 CREATE TABLE IF NOT EXISTS Podcaster (
-	IDPodcaster VARCHAR(5) PRIMARY KEY NOT NULL,
-	NombreArtistico VARCHAR(30) UNIQUE NOT NULL,
+    IDPodcaster VARCHAR(5) PRIMARY KEY NOT NULL,
+    NombreArtistico VARCHAR(30) UNIQUE NOT NULL,
     Imagen VARCHAR(99)
 );
 
@@ -15,69 +15,69 @@ CREATE TABLE IF NOT EXISTS Musico (
 );
 
 CREATE TABLE IF NOT EXISTS Album (
-	IDAlbum VARCHAR(30) PRIMARY KEY NOT NULL,
-    Titulo VARCHAR(30) NOT NULL, 
-    Ano DATE NOT NULL, 
+    IDAlbum VARCHAR(30) PRIMARY KEY NOT NULL,
+    Titulo VARCHAR(30) NOT NULL,
+    Ano DATE NOT NULL,
     Genero VARCHAR(30) NOT NULL,
     Imagen VARCHAR(99),
     IDMusico VARCHAR(5) NOT NULL,
-    FOREIGN KEY (IDMusico)	
-		REFERENCES Musico(IDMusico)
+    FOREIGN KEY (IDMusico)
+        REFERENCES Musico (IDMusico)
 );
 
 CREATE TABLE IF NOT EXISTS Audio (
-	IDAudio VARCHAR(30) PRIMARY KEY UNIQUE NOT NULL,
+    IDAudio VARCHAR(30) PRIMARY KEY UNIQUE NOT NULL,
     Nombre VARCHAR(30) NOT NULL,
-    Duracion TIME NOT NULL, 
-    Imagen VARCHAR(99), 
-    Tipo ENUM('Podcast','Cancion')	
+    Duracion TIME NOT NULL,
+    Imagen VARCHAR(99),
+    Tipo ENUM('Podcast', 'Cancion')
 );
 
 CREATE TABLE IF NOT EXISTS Podcast (
-	IDAudio VARCHAR(30) PRIMARY KEY NOT NULL,
-	Colaboradores VARCHAR(99) NULL,
+    IDAudio VARCHAR(30) PRIMARY KEY NOT NULL,
+    Colaboradores VARCHAR(99) NULL,
     IDPodcaster VARCHAR(5) NOT NULL,
-	FOREIGN KEY (IDAudio)	
-		REFERENCES Audio(IDAudio),
-	FOREIGN KEY (IDPodcaster)	
-		REFERENCES Podcaster(IDPodcaster)	
+    FOREIGN KEY (IDAudio)
+        REFERENCES Audio (IDAudio),
+    FOREIGN KEY (IDPodcaster)
+        REFERENCES Podcaster (IDPodcaster)
 );
 
 CREATE TABLE IF NOT EXISTS Cancion (
-	IDAudio VARCHAR(30) PRIMARY KEY NOT NULL,
-	IDAlbum VARCHAR(30) NOT NULL,
+    IDAudio VARCHAR(30) PRIMARY KEY NOT NULL,
+    IDAlbum VARCHAR(30) NOT NULL,
     ArtistasInvitados VARCHAR(99) NULL,
-	FOREIGN KEY (IDAudio)	
-		REFERENCES Audio(IDAudio),
-	FOREIGN KEY (IDAlbum)	
-		REFERENCES Album(IDAlbum)
+    FOREIGN KEY (IDAudio)
+        REFERENCES Audio (IDAudio),
+    FOREIGN KEY (IDAlbum)
+        REFERENCES Album (IDAlbum)
 );
 
 CREATE TABLE IF NOT EXISTS Cliente (
-	IDCliente VARCHAR(5) PRIMARY KEY NOT NULL,
+    IDCliente VARCHAR(5) PRIMARY KEY NOT NULL,
     Nombre VARCHAR(30) NOT NULL,
     Apellido VARCHAR(30) NOT NULL,
-    Usuario VARCHAR(30) UNIQUE NOT NULL, 
+    Usuario VARCHAR(30) UNIQUE NOT NULL,
     Contrasena VARCHAR(30) NOT NULL,
     FechaNacimiento DATE NOT NULL,
     FechaRegistro DATE NOT NULL,
-    Tipo ENUM('Free','Premium') 
-    );
+    Tipo ENUM('Free', 'Premium')
+);
 
 CREATE TABLE IF NOT EXISTS Premium (
-	IDCliente VARCHAR(5) PRIMARY KEY NOT NULL,
-	FechaCaducidad DATE NOT NULL,
-	FOREIGN KEY (IDCliente)
-		REFERENCES Cliente(IDCliente)
+    IDCliente VARCHAR(5) PRIMARY KEY NOT NULL,
+    FechaCaducidad DATE NOT NULL,
+    FOREIGN KEY (IDCliente)
+        REFERENCES Cliente (IDCliente)
 );
 
 CREATE TABLE IF NOT EXISTS Playlist (
-	IDList VARCHAR(30) PRIMARY KEY NOT NULL,
+    IDList VARCHAR(30) PRIMARY KEY NOT NULL,
     Titulo VARCHAR(30) NOT NULL,
     FechaCreacion DATE NOT NULL,
     IDCliente VARCHAR(5) NOT NULL,
     FOREIGN KEY (IDCliente)
-		REFERENCES Cliente(IDCliente)
+        REFERENCES Cliente (IDCliente)
 );
 
 CREATE TABLE IF NOT EXISTS PlaylistCanciones (
@@ -138,7 +138,7 @@ VALUES
 INSERT IGNORE INTO Podcast(IDAudio, Colaboradores, IDPodcaster)
 VALUES
 ('AUAA2', 'Papa Giorgio', 'PAAA1');
-INSERT IGNORE INTO Cancion(IDAudio, IDAlbum, ArtistasInvitados)
+INSERT IGNORE INTO Cancion(IDAudio, IDAlbum)
 VALUES
 ('AUAA1', 'AAAA1');
 INSERT IGNORE INTO Cliente(IDCliente, Nombre, Apellido, Usuario, Contrasena, FechaNacimiento, FechaRegistro, Tipo)
