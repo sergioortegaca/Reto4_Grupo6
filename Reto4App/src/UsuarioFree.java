57e195cb1ebd2265e9720cd64b22c553a2ed032e
in1dm3 <in1dm3@HZ352011.elorrieta.int>
2024-04-23 14:26:39 +0200

    Actualización objetos.

--- Reto4App/src/UsuarioFree.java
+++ Reto4App/src/UsuarioFree.java
@@ -1,2 +1,7 @@
 public class UsuarioFree extends Usuario {
+
+	public UsuarioFree(int clienteID, String nombre, String apellido, String usuario, String contrasena,
+			String fechaNacimiento, String fechaRegistro) {
+		super();
+	}
 }
