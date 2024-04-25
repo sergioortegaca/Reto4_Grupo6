0ebf03c13d9326f8becdd4bfe4984bc74e98f6f4
in1dm3 <in1dm3@HZ352026.elorrieta.int>
2024-04-25 13:19:02 +0200

    Mensaje Menú

--- Reto4App/src/Metodos.java
+++ Reto4App/src/Metodos.java
@@ -1,6 +1,9 @@
 import java.awt.CardLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import javax.swing.JButton;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
@@ -37,5 +40,22 @@ public class Metodos {
 		});
 		btnAtras.setBounds(55, 39, 89, 23);
 		variablePanel.add(btnAtras);
+	}
+
+	public String bienvenidaMenu() {
+		String msgBienvenida = "";
+
+		Calendar rightNow = Calendar.getInstance();
+		int hour = rightNow.get(Calendar.HOUR_OF_DAY);
+
+		if (hour < 6 || hour == 21 || hour == 22 || hour == 23)
+			msgBienvenida = "¡Buenas noches!";
+		else if (hour >= 6 && hour < 12)
+			msgBienvenida = "¡Buenos días!";
+		else
+			msgBienvenida = "¡Buenas tardes!";
+
+		return msgBienvenida;
+
 	}
 }
--- Reto4App/src/Reto4Main.java
+++ Reto4App/src/Reto4Main.java
@@ -381,7 +381,7 @@ public class Reto4Main extends JFrame {
 		JButton btnDescubrirPodcasts = new JButton("Descubrir podcasts");
 		btnDescubrirPodcasts.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				metodos.cambiarDePanel(layeredPane, "DescubrirPoscasts");
+				metodos.cambiarDePanel(layeredPane, "DescubrirPodcasts");
 			}
 		});
 		btnDescubrirPodcasts.setBounds(304, 219, 265, 23);
@@ -398,12 +398,10 @@ public class Reto4Main extends JFrame {
 		btnNewButton_4.setBounds(304, 265, 265, 23);
 		panelMenu.add(btnNewButton_4);
 
-		
-		
-		JLabel lblMenu = new JLabel("");
+		JLabel lblMenu = new JLabel(metodos.bienvenidaMenu());
 		lblMenu.setHorizontalAlignment(SwingConstants.CENTER);
-		lblMenu.setFont(new Font("Tahoma", Font.BOLD, 50));
-		lblMenu.setBounds(287, 19, 299, 141);
+		lblMenu.setFont(new Font("Tahoma", Font.BOLD, 40));
+		lblMenu.setBounds(262, 59, 349, 83);
 		panelMenu.add(lblMenu);
 
 		JPanel panelDescubrirMusica = new JPanel();
@@ -413,7 +411,7 @@ public class Reto4Main extends JFrame {
 
 		JPanel panelDescubirPodcasts = new JPanel();
 		panelDescubirPodcasts.setBackground(new Color(255, 255, 255));
-		layeredPane.add(panelDescubirPodcasts, "DescubirPodcasts");
+		layeredPane.add(panelDescubirPodcasts, "DescubrirPodcasts");
 		panelDescubirPodcasts.setLayout(null);
 
 		JPanel panelMisPlaylists = new JPanel();
